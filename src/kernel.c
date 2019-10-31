a99ddd64cf0da22a7e499b9f8212123d8f856c3a
Jamason Davis <jpdavis2471@gmail.com>
2019-10-31 10:27:01 -0500

    Fixed whitespace

--- src/kernel.c
+++ src/kernel.c
@@ -5,7 +5,7 @@
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
- 
+
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
@@ -29,33 +29,33 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
- 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
- 
-size_t strlen(const char* str) 
+
+size_t strlen(const char* str)
 {
 	size_t len = 0;
 	while (str[len])
 		len++;
 	return len;
 }
- 
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
- 
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
- 
-void terminal_initialize(void) 
+
+void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
@@ -68,19 +68,19 @@ void terminal_initialize(void)
 		}
 	}
 }
- 
-void terminal_setcolor(uint8_t color) 
+
+void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
- 
-void terminal_putchar(char c) 
+
+void terminal_putchar(char c)
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
@@ -89,20 +89,20 @@ void terminal_putchar(char c)
 			terminal_row = 0;
 	}
 }
- 
-void terminal_write(const char* data, size_t size) 
+
+void terminal_write(const char* data, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
- 
-void terminal_writestring(const char* data) 
+
+void terminal_writestring(const char* data)
 {
 	terminal_write(data, strlen(data));
 }
- 
-void kernel_main(void) 
+
+void kernel_main(void)
 {
-	terminal_initialize(); 
+	terminal_initialize();
 	terminal_writestring("Hello world!\n");
 }
