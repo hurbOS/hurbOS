37e206a4c733c08fa0b1c6a53334683c06c5e57b
Jamason Davis <jpdavis2471@gmail.com>
2020-04-03 11:13:23 -0500

    Not compiling, but GDT code

---
+++ kernel/arch/i386/gdt_as.s
@@ -0,0 +1,15 @@
+.global gdt_flush
+
+gdt_flush:
+   mov (esp+4), %eax
+   lgdt (eax)
+
+   mov 0x10, %ax    
+   mov %ax, %ds      
+   mov %ax, %es
+   mov %ax, %fs
+   mov %ax, %gs
+   mov %ax, %ss
+   jmp 0x08:_flush 
+.flush:
+   ret
